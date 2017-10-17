f705047df2be02e0dd861f24ce1663dd0d8ad1bb
luke.doll <31712880+ludoll@users.noreply.github.com>
2017-10-17 17:10:06 -0500

    First attempt at a redo

--- main.py
+++ main.py
@@ -1,28 +1,93 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+from cgi import escape
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:3306/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blog_id = db.relationship('blog_id', backref='owner')
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
 
 class Blog(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), unique=True)
     body = db.Column(db.String(1000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
        
     def __init__(self, title, body):
         self.title = title
-        self.body= body
+        self.body = body
+        self.owner = owner
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            session['email'] = email
+            flash('Successfully logged in!')            
+            return redirect('/')
+        else:
+            flash('Either your password is wrong or you are not real','error')
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST','GET'])
+def register():
+    password = request.form['password']
+    verify = request.form['verify']
+    email = request.form['email']
+
+    password = escape(password) 
+    verify = escape(verify)
+    email = escape(email)
+
+    password_error = ""
+    verify_error = ""
+    email_error = ""
+
+    if password == "" or " " in password or len(password) < 3 or len(password) > 20:
+        password_error = "Your password isn't good enough, friend"
+    if verify == "" or verify != password:
+        verify_error = "Your passwords don't match"
+    if "@" not in email or "." not in email or " " in email or len(email) < 3 or len(email) > 30:
+        email_error = "This can't be a real email address."
+        
+
+    if email_error == "" and verify_error == "" and password_error == "":
+        return render_template("welcome.html")
+    else:
+        return render_template("index.html", password_error = password_error
+                                           , verify_error = verify_error
+                                           , email_error = email_error
+                                           , username = username
+                                           , email = email
 
 @app.route('/newpost', methods=['GET', 'POST'])
 def add_blog():
     if request.method == 'GET':
-        return render_template('newpost.html', title='Add a Blog Entry')
-
+        return render_template('newpost.html')
 
     if request.method == 'POST':
         blog_title = request.form['title']
